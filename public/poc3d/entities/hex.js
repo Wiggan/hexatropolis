23feeeddcc51aa935c9d972d22f092d0fba03968
Fredrik Wigert <wigert@gmail.com>
2022-01-24 11:23:45 +0100

    Chipped the hex model

--- public/poc3d/entities/hex.js
+++ public/poc3d/entities/hex.js
@@ -4,5 +4,10 @@ class Hex extends Drawable {
     constructor(parent, local_position) {
         super(parent, local_position, models.hex);
         this.material = materials.dirt;
+        this.local_transform.yaw(Math.floor(Math.random()*6)*60);
+        if (Math.random() < 0.5) {
+            this.local_transform.roll(180);
+            this.local_transform.translate([0, -4, 0]);
+        }
     }
 }
